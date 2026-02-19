0e58d33c1f9ed1abcf6f3f2aeffe014d682072ff
Mark Castle <mark.castle.1415@gmail.com>
2026-02-18 19:30:46 -0700

    Added ability to delete by Stand ID, Stand CN, and CaseID in delete_caseid function. Outer for loop was also removed and CaseID values are selected using IN operator.

--- R/run-key.R
+++ R/run-key.R
@@ -119,19 +119,21 @@ run_key = function(dll_path = "C:/FVS/FVSSoftware/FVSbin",
 #'This function deletes desired rows in all database tables containing a CaseID 
 #'field from a specified output FVS database. The rows that are deleted are 
 #'determined from a set of CaseID values associated with any of the following
-#'attributes: FVS run title, management ID, FVS variant code, or keyword file. 
-#'The case IDs identified for deletion are extracted from the FVS_Cases table 
-#'from the output FVS database. Some of the logic in this function was borrowed 
-#'from Nick Crookston's deleteRelatedDBRows function from fvsRunUtilities.R 
-#'file (fvsOL R package).
+#'attributes: FVS run title, management ID, FVS variant code, keyword file name
+#'set of stand ID(s), set of stand CN(s), or a set of CaseID values. The case 
+#'IDs identified for deletion are extracted from the FVS_Cases table from the 
+#'output FVS database. Some of the logic in this function was borrowed from Nick
+#'Crookston's deleteRelatedDBRows function from fvsRunUtilities.R file (fvsOL R
+#' package).
 #
 #'@param fvsout: 
 #'Character string corresponding to file path to output FVS SQLite database
 #'(.db, .sqlite).
 #
 #'@param delete_id: 
-#'Vector of character strings pertaining to an FVS run title, management ID, FVS
-#'variant code, or file path to keyword.
+#'Vector of character strings pertaining to FVS run title(s), management ID(s), 
+#'FVS variant code(s), keyword file name(s), stand ID values, stand CN values,
+#'or Case ID values.
 #'
 #'@param id_type:
 #'Integer value pertaining to type of value specified in delete_id argument.
@@ -143,6 +145,12 @@ run_key = function(dll_path = "C:/FVS/FVSSoftware/FVSbin",
 #'3 = management id (e.g. from MgmtID in FVS_Cases table)
 #'
 #'4 = FVS variant code (e.g. from Variant in FVS_Cases table)
+#'
+#'5 = Stand ID values (e.g. StandID in FVS_Cases table)
+#'
+#'6 = Stand CN values (e.g. Stand_CN in FVS_Cases table)
+#'
+#'7 = Case ID values (e.g. CaseID in FVS_Cases table)
 #
 #'@param vacuum: 
 #'Logical variable used to determine if output database should be vacuumed after
@@ -185,17 +193,22 @@ delete_caseid = function(fvsout = NULL,
     stop("No value(s) specified for delete_id.")
   
   #Set id_type to 1 if bad value is entered
-  if(!id_type %in% c(1:4)) id_type = 1
+  if(!id_type %in% c(1:7)) id_type = 1
   
   #Set select_var based on id_type
-  select_var <- switch(id_type,
-                       'KeywordFile',
-                       'RunTitle',
-                       'MgmtID',
-                       'Variant')
+  select_var = switch(id_type,
+                      'KeywordFile',
+                      'RunTitle',
+                      'MgmtID',
+                      'Variant',
+                      'StandID',
+                      'Stand_CN',
+                      'CaseID')
   
   #Connect to output database
   con_out = RSQLite::dbConnect(RSQLite::SQLite(), fvsout)
+  on.exit(try(if(RSQLite::dbIsValid(con_out)) RSQLite::dbDisconnect(con_out), 
+              silent = TRUE))
   
   #Get output database tables
   out_tbl = RSQLite::dbListTables(conn = con_out)
@@ -208,44 +221,43 @@ delete_caseid = function(fvsout = NULL,
     stop("FVS_Cases table was not found in output database.")
   }
   
-  #Start loop across values in delete_id
-  for(val in delete_id)
-  {
-    
-    if(verbose)
-    {
-      cat("id_type:", id_type, "\n")
-      cat("Variable used for case ID selection:", select_var, "\n")
-      cat("Value for case ID deletion:", val, "\n")
-    }
-    
-    #CaseID query
-    query = paste("SELECT CaseID FROM FVS_Cases WHERE",
-                   select_var,
-                   "=", 
-                   paste0("'", val, "'"),
-                   "COLLATE NOCASE")
-    
-    #Attempt to get CaseIDs...
-    caseid = RSQLite::dbGetQuery(conn = con_out,
-                                  query)[[1]]
+  #Collapse delete_id into single string
+  id_string = collect_id(delete_id)
   
-    if(verbose)
-    {
-      cat("CaseID Query:", query, "\n")
-      cat("Number of Case ID values selected:", length(caseid), "\n", "\n")
-    }
+  if(verbose)
+  {
+    cat("id_type:", id_type, "\n")
+    cat("Variable used for case ID selection:", select_var, "\n")
+    cat("Value for case ID deletion:", id_string, "\n")
+  }
     
-    #If no case id values are found, skip to next iteration.
-    if(length(caseid) <= 0)
-    {
-      cat("No Case ID values found for deletion using:", val, "\n", "\n")
-      next
-    }
+  #CaseID query
+  query = paste("SELECT CaseID FROM FVS_Cases WHERE",
+                select_var,
+                "COLLATE NOCASE",
+                "IN", 
+                id_string)
     
+  #Attempt to get CaseIDs...
+  caseid = RSQLite::dbGetQuery(conn = con_out,
+                               query)[[1]]
+  
+  if(verbose)
+  {
+    cat("CaseID Query:", query, "\n")
+    cat("Number of Case ID values selected:", length(caseid), "\n", "\n")
+  }
+    
+  #No case ID values found
+  if(length(caseid) <= 0)
+    cat("No Case ID values found for deletion using value(s) specified for:",
+        select_var, "\n", "\n")
+  
+  #Delete identified CaseID values
+  else
+  {
     #Collect caseid in single string 
-    caseid = paste0("'", caseid, "'")
-    case_string = paste0("(", paste(caseid, collapse = ", "), ")")
+    case_string = collect_id(caseid)
     
     #Loop across tables and delete rows
     for(tbl in out_tbl)
@@ -254,19 +266,19 @@ delete_caseid = function(fvsout = NULL,
       
       #Remove rows if table contains CaseID field
       if("CaseID" %in% RSQLite::dbListFields(conn = con_out,
-                                                name = tbl))
+                                             name = tbl))
       {
         #Create delete query
         query = paste0("DELETE FROM ",
-                        tbl, 
-                        " WHERE ",
-                        tbl,
-                        ".CaseID IN",
-                        case_string)
+                       tbl, 
+                       " WHERE ",
+                       tbl,
+                       ".CaseID IN",
+                       case_string)
         
         #Execute query
         rtn = RSQLite::dbExecute(conn = con_out,
-                                  query)
+                                 query)
         
         if(verbose) cat("Number of rows deleted:", rtn, "\n")
         
@@ -287,7 +299,7 @@ delete_caseid = function(fvsout = NULL,
       }
     }
   }
-  
+    
   #Run vacuum?
   if(vacuum)
   {
@@ -300,7 +312,3 @@ delete_caseid = function(fvsout = NULL,
   
   invisible()
 }
-
-# delete_caseid(fvsout = "C:/FVS/Project_1/FVSOut.db",
-#               delete_id = c("ak", "pn", "wc"),
-#               id_type = 4)
