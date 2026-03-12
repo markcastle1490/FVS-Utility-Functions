31d4422c9913289a20c69c2802bc684978e5d851
markcastle1415 <105014784+markcastle1415@users.noreply.github.com>
2026-03-12 10:32:20 -0600

    Updates to linint function.

--- R/linint.R
+++ R/linint.R
@@ -5,54 +5,72 @@
 #'This function is used to mimic the linint function from the FVS event monitor.
 #
 #'@param value:
-#' Input value (numeric) that is being evaluated by the linint interpolation
-#' logic.
+#'Input value (numeric) that is being evaluated by the linint interpolation
+#' ogic.
 #
 #'@param x:     
-#'Numeric Vector containing the x variables evaluated in the linint function.
+#'Numeric vector containing the x variables evaluated in the linint function.
 #
 #'@param y:     
-#'Numeric Vector containing the x variables evaluated in the linint function.
+#'Numeric vector containing the x variables evaluated in the linint function.
+#
+#'@param verbose: 
+#'Logical variable used to determine if debug output should be echoed to 
+#'console.
 #
 #'@return
 #'Numeric value determined by the linear interpolation.
 ################################################################################ 
 
 #'@export
-linint <- function(value, x, y)
+linint <- function(value = 0, 
+                   x = NULL, 
+                   y = NULL,
+                   verbose = FALSE)
 {
+  algslp = 0
+  
   #If the length of x and y variables are not equal return with value of 0
-  if(length(x) != length(y))
-  {
-    algslp = 0
+  if(length(x) <= 0 || length(y) <= 0 || (length(x) != length(y)))
     return(algslp)
-  }
+  
+  #Cast value, x and y to numeric if needed
+  if(!is.numeric(value)) value = is.numeric(value)
+  if(!is.numeric(x)) x = is.numeric(x)
+  if(!is.numeric(y)) y = is.numeric(y)
   
   #If value is less than first value in x, return the first value in y
   if(value < x[1])
-  {
     algslp = y[1]
-  }
   
   #If value is greater than or equal to last value in x, return the last value
   #in y
   else if(value >= x[length(x)])
-  {
     algslp = y[length(y)]
-  }
   
   #Begin interpolation logic
   else
   {
     #Loop across length of x vector minus one.
-    for(i in 1:length(x) - 1)
+    for(i in 1:(length(x) - 1))
     {
+      if(verbose)
+        cat("value:", value, "\n",
+            "i:", i, "\n",
+            "x[i]:", x[i], "\n",
+            "y[i]:", y[i], "\n",
+            "x[i + 1]:", x[i + 1], "\n",
+            "y[i + 1]:", y[i + 1], "\n", "\n")
+      
       #If value is greater than or equal to next value in x, skip the next
       #iteration of the loop
       if(value >= x[i + 1]) next
       
       #Else, retrieve the interpolated value and break from loop
-      algslp = y[i]+((y[i+1]-y[i])/(x[i+1]-x[i]))*(value-x[i])
+      algslp = y[i] + ((y[i + 1] - y[i])/(x[i + 1] - x[i])) * (value - x[i])
+      if(verbose)
+        cat("y[i] + ((y[i + 1] - y[i])/(x[i + 1] - x[i])) * (value - x[i]) = ", 
+            algslp, "\n")
       break
     }
   }
